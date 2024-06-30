06f07f17164f141326162b9825c6ae12fdce2b69
Immanuel <62933873+immanuel-23@users.noreply.github.com>
2024-06-30 23:34:29 +0530

    Add files via upload
    First commit for rust

---
+++ Primitive_Data_Type/dataType.rs
@@ -0,0 +1,18 @@
+fn main(){
+    println!("Hello Test");
+    //int 
+    let x :i32=42;
+    let y: i32 =6;
+    let k: u32 =6;
+   println!("The value of x is: {}",x);
+   println!("The Value of y is :{}",y);
+   println!("The Value of k is :{}",k);
+   println!("The additon  of x and Y is :{}",x+y);
+    let z :f32=42.001231333;
+    println!("The value of z  is :{}",z);
+
+    //boolean
+    let is_available :bool =true;
+    println!("Is Avaliable {}",is_available)
+   
+}
