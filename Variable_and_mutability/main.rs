ef07ae89bc6b90e75f697316041ba5ec8ee5f66f
Immanuel <62933873+immanuel-23@users.noreply.github.com>
2024-07-05 19:01:52 +0530

    Add files via upload
    variable and mutability

---
+++ Variable_and_mutability/main.rs
@@ -0,0 +1,7 @@
+fn main(){
+    println!("Variable and mutabality");
+    let mut  a=6;
+    println!("The value of a is {}",a);
+    a=a+1;
+    println!("The value of a after addition is {}",a);
+}
