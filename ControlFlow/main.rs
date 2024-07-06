34c7949fb229014f14b4d04a5605ee886366a35d
Immanuel <62933873+immanuel-23@users.noreply.github.com>
2024-07-06 12:25:35 +0530

    Add files via upload
    if else and else if

---
+++ ControlFlow/main.rs
@@ -0,0 +1,10 @@
+fn main(){
+    println!("Control Flow");
+
+    let age:i32=18;
+    if age>18 {
+        println!("Ur allowed to drive a car");
+    }else{
+        println!("u can't drive a car");
+    }
+}
