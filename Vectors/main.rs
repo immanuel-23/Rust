25eb359988703af995f4c2d9275a4622cd78ccf5
Immanuel <62933873+immanuel-23@users.noreply.github.com>
2024-07-14 11:25:11 +0530

    Add files via upload
    vectors in rust

---
+++ Vectors/main.rs
@@ -0,0 +1,16 @@
+fn main(){
+    println!("Vectors in Rust");
+    let mut _v:Vec<i32>=Vec::new();
+    _v.push(5);
+    _v.push(6);
+    _v.push(7);
+    _v.push(8);
+    _v.push(9);
+   
+    println!("v {}",_v[0]);
+    println!("v {}",_v[1]);
+    println!("v {}",_v[2]);
+    println!("v {}",_v[3]);
+
+    
+}
